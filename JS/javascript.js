a63c9b0775b8510687b09727a62c6c3b35c709f2
Maximiliano Reszczynski <masanta.dwfsc3@bootcampudd.cl>
2022-10-21 15:33:07 -0300

    Empezando con JS
    Creo mis primeras variables y funciones.
    Almacenamiento en LocalStorage

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,1 +1,38 @@
+const formularioUI = document.querySelector('#formulario');
+const listaActividadesUI = document.querySelector('#listaActividades');
+let arrayActividades = [];
 
+const CrearItem = () => {
+    let item = {
+        actividad: '',
+        estado: false 
+    }
+    arrayActividades.push(item);
+    return item;
+     
+}
+const GuardarDB = (actividad)=>{
+
+    localStorage.setItem('nombres',JSON.stringify(arrayActividades)); 
+
+}
+
+const LeerDB = () =>{
+
+}
+
+
+formularioUI.addEventListener('submit', (e) =>{
+
+    e.preventDefault();
+    let actividadUI = document.querySelector ('#actividad').value;
+    CrearItem(actividadUI)
+    GuardarDB();
+
+    formularioUI.reset();
+
+})
+
+document.addEventListener('DOMContentLoaded', LeerDB);
+
+ 
